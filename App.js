78c2aefee58d9dd34289c21c877eb76746d9d814
ddddddO <lbfdeatq@gmail.com>
2023-03-11 18:10:32 +0900

    init

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, SafeAreaView, TextInput, Button, Alert } from 'react-native';
 
 export default function App() {
+  const [text, onChangeText] = React.useState('Please input text here!!!');
+
+  const handleSubmit = () => {
+    Alert.alert('Send text!:', text);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Open up App.js to start working on your app!!!!</Text>
+      <SafeAreaView>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeText}
+          value={text}
+        />
+        <Button
+          title="Press me"
+          color="#f194ff"
+          onPress={handleSubmit}
+        />
+      </SafeAreaView>
     </View>
   );
 }
---
+++ README.md
@@ -0,0 +1,21 @@
+# MEMO
+- local working directory
+  ```console
+  PS C:\Users\lbfde> cd .\ultrachat-front\
+  PS C:\Users\lbfde\ultrachat-front> code .
+  ```
+
+- make project
+  - `npx create-expo-app ultrachat-front`
+
+- dependencies
+  ```console
+  npx expo install react-native-web@~0.18.10 react-dom@18.2.0 @expo/webpack-config@^18.0.1 # webでも使いたいため
+  ```
+
+- start
+  - `npx expo start`
+
+# Images
+![](_images/35860.jpg)
+![](_images/35861.jpg)
